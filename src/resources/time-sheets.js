fa58b84e2ea3f48bfa1582453717a18993b8cdb6
LautaroCaballero <86745664+LautaroCaballero@users.noreply.github.com>
2022-05-08 13:29:44 -0300

    TG-08: add a timesheet function added

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,55 +1,67 @@
 const express = require('express');
-const res = require('express/lib/response');
-const timesheet = require('../data/time-sheets.json');
+const fs = require('fs');
+const timesheets = require('../data/time-sheets.json');
 
 const router = express.Router();
 
 router.get('/getAll', (req, res) => {
-  res.send(timesheet);
+  res.send(timesheets);
 });
 
 router.get('/getByTimesheet/:id', (req, res) => {
   const timesheetID = req.params.id;
-  const user = timesheet.find((timesheet) => timesheet.id === timesheetID);
+  const user = timesheets.find((timesheet) => timesheet.id === timesheetID);
 
   if (user) {
     res.send(user);
   } else {
-    res.send('User not found');
+    res.send(`Timesheet ${timesheetID} not found`);
   }
 });
 
 router.get('/getByRole/:role', (req, res) => {
   const userRol = req.params.role;
-  const user = timesheet.filter((timesheet) => timesheet.role === userRol);
+  const user = timesheets.filter((timesheet) => timesheet.role === userRol);
 
   if (user) {
     res.send(user);
   } else {
-    res.send('User not found');
+    res.send(`Users with ${userRol} rol not found`);
   }
 });
 
 router.get('/getByProject/:project', (req, res) => {
   const { project } = req.params;
-  const allProject = timesheet.filter((timesheet) => timesheet.project === project);
+  const allProject = timesheets.filter((timesheet) => timesheet.project === project);
 
   if (allProject) {
     res.send(allProject);
   } else {
-    res.send('Project not found');
+    res.send(`Project ${project} not found`);
   }
 });
 
 router.get('/getByEmployeeID/:employeeID', (req, res) => {
   const employee = req.params.employeeID;
-  const filterID = timesheet.find((timesheet) => timesheet.employeeID === employee);
+  const filterID = timesheets.find((timesheet) => timesheet.employeeID === employee);
 
   if (filterID) {
     res.send(filterID);
   } else {
-    res.send('User not found');
+    res.send(`User ${employee} not found`);
   }
+});
+
+router.post('/Add', (req, res) => {
+  const timesheetData = req.body;
+  timesheets.push(timesheetData);
+  fs.writeFile('./src/data/time-sheets.json', JSON.stringify(timesheets), (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Timesheet added');
+    }
+  });
 });
 
 module.exports = router;
