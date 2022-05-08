9bd5af889c0aa73dea1fd9047fc360aea145a955
LautaroCaballero <86745664+LautaroCaballero@users.noreply.github.com>
2022-05-08 16:31:30 -0300

    TG-08: edit a timesheet function added

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const res = require('express/lib/response');
 const fs = require('fs');
 const timesheets = require('../data/time-sheets.json');
 
@@ -84,6 +83,31 @@ router.delete('/delete/:id', (req, res) => {
         }
       },
     );
+  }
+});
+
+router.put('/edit/:id', (req, res) => {
+  let userID = req.params.id;
+  const jsonData = fs.readFileSync('src/data/time-sheets.json');
+  const data = JSON.parse(jsonData);
+  const filterID = timesheets.find((timesheet) => timesheet.id === userID);
+  if (filterID) {
+    userID -= 1;
+    data[userID].employeeID = req.body.employeeID;
+    data[userID].name = req.body.name;
+    data[userID].surname = req.body.surname;
+    data[userID].role = req.body.role;
+    data[userID].startDate = req.body.startDate;
+    data[userID].finishDate = req.body.finishDate;
+    data[userID].regularHours = req.body.regularHours;
+    data[userID].overtimeHours = req.body.overtimeHours;
+    data[userID].rate = req.body.rate;
+    data[userID].project = req.body.project;
+    data[userID].task = req.body.task;
+    fs.writeFileSync('src/data/time-sheets.json', JSON.stringify(data));
+    res.json(data);
+  } else {
+    res.send('User not found');
   }
 });
 
