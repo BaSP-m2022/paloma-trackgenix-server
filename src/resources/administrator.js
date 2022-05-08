02ea01e505b926851fd86c1038cc6f795c2ad620
Hernan Cazeneuve <hernancazeneuve@gmail.com>
2022-05-08 20:44:08 -0300

    TG-08: Finished delete and edit

--- src/resources/administrator.js
+++ src/resources/administrator.js
@@ -43,10 +43,10 @@ router.get('/getByStatus/:status', (req, res) => {
 // Creation of a new admin.
 router.post('/add', (req, res) => {
   const adminData = req.body;
-  /* Validation in process, will be completed after finishing the exercise
+  /* // Validation in process, will be completed after finishing the exercise
    if (!adminData.id || !adminData.name || !adminData.lastName
     || !adminData.email || !adminData.gender || !adminData.status) {
-    alert('All the fields must be completed');
+    res.send('Admin successfully deleted');('All the fields must be completed');
   } */
   administrators.push(adminData);
   fs.writeFile('src/data/admins.json', JSON.stringify(administrators), (err) => {
@@ -57,5 +57,40 @@ router.post('/add', (req, res) => {
     }
   });
 });
-
+// Delete of an existing admin.
+router.delete('/delete/:id', (req, res) => {
+  const adminId = req.params.id;
+  const filteredAdmins = administrators.filter((eachAdm) => eachAdm.id !== adminId);
+  if (filteredAdmins.length === administrators.length) {
+    res.send(`Admin ${adminId} not found`);
+  } else {
+    fs.writeFile('src/data/admins.json', JSON.stringify(filteredAdmins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Admin successfully deleted');
+      }
+    });
+  }
+});
+// Edit users parameters.
+router.put('/edit/:id', (req, res) => {
+  let adminId = req.params.id;
+  const jsonData = fs.readFileSync('src/data/admins.json');
+  const data = JSON.parse(jsonData);
+  const filteredAdmins = administrators.find((eachAdm) => eachAdm.id === adminId);
+  if (filteredAdmins) {
+    adminId -= 1;
+    data[adminId].adminId = req.body.adminId;
+    data[adminId].name = req.body.name;
+    data[adminId].lastName = req.body.lastName;
+    data[adminId].email = req.body.email;
+    data[adminId].gender = req.body.gender;
+    data[adminId].status = req.body.status;
+    fs.writeFileSync('src/data/admins.json', JSON.stringify(data));
+    res.json(data);
+  } else {
+    res.send('User not found');
+  }
+});
 module.exports = router;
