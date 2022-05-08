8b3c1936821cf76ef2aaa912a7e9119e2487bc5c
Maria Ana Marraccini <mariaanamarraccini@gmail.com>
2022-05-08 01:27:37 -0300

    TG-08: Changes in the index

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('super-admins', superAdminRouter);
-
+app.use('/super-admins', superAdminRouter);
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
