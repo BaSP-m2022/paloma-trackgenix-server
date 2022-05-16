708017bacfb8487912eb61d9a8c36c4b5bfbc05f
Carla Daiana Vigil <carvigil21@gmail.com>
2022-05-16 12:18:28 -0300

    TG-31: Added CRUD methods

--- src/controllers/administrator.js
+++ src/controllers/administrator.js
@@ -1,98 +1,112 @@
-// Calling exrpess.
-const express = require('express');
-// File System (library).
-const fs = require('fs');
-// Array of admins.
-const administrators = require('../data/admins.json');
-// Defining Router
-const router = express.Router();
-// gets all data in Json file
-router.get('/', (req, res) => {
-  res.send(administrators);
-});
-// gets one admin by its id when find is used it only finds the 1st one
-router.get('/:id', (req, res) => {
-  const adminId = req.params.id;
-  const administrator = administrators.find((eachAdm) => eachAdm.id === adminId);
-  if (administrator) {
-    res.send(administrator);
-  } else {
-    res.send('Admin not found');
-  }
-});
-// filter list by gender
-router.get('/getByGender/:gender', (req, res) => {
-  const adminGender = req.params.gender;
-  const filteredAdmins = administrators.filter((eachAdm) => eachAdm.gender === adminGender);
-  if (filteredAdmins.length > 0) {
-    res.send(filteredAdmins);
-  } else {
-    res.send(`There are no ${adminGender} Admins`);
-  }
-});
-// filter list by Status active
-router.get('/getByStatus/:status', (req, res) => {
-  const adminStatus = req.params.status;
-  const filteredAdmins = administrators.filter((eachAdm) => eachAdm.status === adminStatus);
-  if (filteredAdmins) {
-    res.send(filteredAdmins);
-  } else {
-    res.send('There are no active Admins');
-  }
-});
-// Creation of a new admin.
-router.post('/', (req, res) => {
-  const adminData = req.body;
-  const adminFound = administrators.find((eachAdm) => eachAdm.id === adminData.id);
-  // Validation in process, will be completed after finishing the exercise
-  if (adminData.id && adminData.name && adminData.lastName
-    && adminData.email && adminData.gender && adminData.status && !adminFound) {
-    administrators.push(adminData);
-    fs.writeFile('src/data/admins.json', JSON.stringify(administrators), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send('User created');
-      }
-    });
-  } else {
-    res.send('All fields must be completed or the id already is taken');
-  }
-});
-// Delete of an existing admin.
-router.delete('/:id', (req, res) => {
-  const adminId = req.params.id;
-  const filteredAdmins = administrators.filter((eachAdm) => eachAdm.id !== adminId);
-  if (filteredAdmins.length === administrators.length) {
-    res.send(`Admin ${adminId} not found`);
-  } else {
-    fs.writeFile('src/data/admins.json', JSON.stringify(filteredAdmins), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send('Admin successfully deleted');
-      }
+import Admin from '../models/Admin';
+
+// eslint-disable-next-line consistent-return
+const getAllAdmins = async (req, res) => {
+  try {
+    const allAdmins = await Admin.find({});
+    return res.status(200).json(allAdmins);
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was an error getting all admins',
+      data: {},
+      error: true,
     });
   }
-});
-// Edit users parameters.
-router.put('/:id', (req, res) => {
-  let adminId = req.params.id;
-  const jsonData = fs.readFileSync('src/data/admins.json');
-  const data = JSON.parse(jsonData);
-  const filteredAdmins = administrators.find((eachAdm) => eachAdm.id === adminId);
-  if (filteredAdmins) {
-    adminId -= 1;
-    data[adminId].adminId = req.body.adminId;
-    data[adminId].name = req.body.name;
-    data[adminId].lastName = req.body.lastName;
-    data[adminId].email = req.body.email;
-    data[adminId].gender = req.body.gender;
-    data[adminId].status = req.body.status;
-    fs.writeFileSync('src/data/admins.json', JSON.stringify(data));
-    res.json(data);
-  } else {
-    res.send('User not found');
+};
+
+const getAdminById = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const admin = await Admin.findById(req.params.id);
+      return res.status(200).json(admin);
+    }
+    return res.status(400).json({
+      message: 'Missing Id parameter',
+      data: {},
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'There was an error getting an admin by id',
+      data: {},
+      error: true,
+    });
   }
-});
-module.exports = router;
+};
+
+const createAdmin = async (req, res) => {
+  try {
+    const admin = new Admin({
+      name: req.body.name,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      gender: req.body.gender,
+      active: req.body.active,
+    });
+    const result = await admin.save();
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'There was an error creating admin',
+      data: {},
+      error: true,
+    });
+  }
+};
+
+const editAdmin = async (req, res) => {
+  try {
+    if (!req.params) {
+      return res.status(400).json({
+        message: 'Missing id parameter',
+      });
+    }
+    const result = await Admin.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    if (!result) {
+      return res.status(404).json({
+        message: 'The admin was not found',
+      });
+    }
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.json({
+      message: 'There was an error editing the admin',
+      error: error.details[0].message,
+    });
+  }
+};
+
+const deleteAdmin = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      return res.status(400).json({
+        msg: 'Missing id parameter',
+      });
+    }
+    const result = await Admin.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        msg: 'The admin was not found',
+      });
+    }
+    return res.status(200).json({
+      msg: 'Deleted the admin!',
+    });
+  } catch (error) {
+    return res.json({
+      msg: 'There was an error deleting the admin',
+    });
+  }
+};
+
+export default {
+  getAllAdmins,
+  getAdminById,
+  createAdmin,
+  editAdmin,
+  deleteAdmin,
+};
