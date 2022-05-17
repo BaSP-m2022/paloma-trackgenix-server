751855fac469c96b104a8286a1dcfcbe2a965049
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-05-17 11:54:04 -0300

    TG-13: data project.json deleted

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// With this line and the body parser the server can obtain info from postman
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +22,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
