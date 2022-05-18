d5e6cefadb9b7a96b0bb107381c14782ff0f5231
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-05-18 17:30:36 -0300

    TG-13: Corrections on projects controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import projectsRoute from './projects';
+import employeesRoutes from './employees';
 
 const router = express.Router();
 
 router.use('/projects', projectsRoute);
-
-import employeesRoutes from './employees';
-
-const router = express.Router();
 
 router.use('/employees', employeesRoutes);
 
