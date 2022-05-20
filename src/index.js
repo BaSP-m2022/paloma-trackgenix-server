cfedeb04e5715634fa0368cc567d4546249fa7b9
leonel listro <leonellistro@gmail.com>
2022-05-20 19:46:59 -0300

    TG-47: app.js and seed of admins created and edited

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import mongoose from 'mongoose';
-import express from 'express';
-import bodyParser from 'body-parser';
-import router from './routes';
+import app from './app';
+
+const port = process.env.PORT || 3000;
 
 const mongoDBURL = 'mongodb+srv://BaSP:BaSP2022@cluster0.caoft.mongodb.net/BaSP-database?appName=mongosh+1.3.1';
 mongoose.connect(mongoDBURL, () => {
   console.log('Connected to the database');
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 }, (error) => console.log(`Failed to connect to the database ${error}`));
-
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
