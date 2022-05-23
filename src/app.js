0068837fc4a17ed6c6728a4e0cc5ba259691fc8a
LautaroCaballero <86745664+LautaroCaballero@users.noreply.github.com>
2022-05-23 11:01:50 -0300

    TG-51: Tasks seed created

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser';
 import router from './routes';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -11,10 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 export default app;
--- src/seeds/tasks.js
+++ src/seeds/tasks.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default [{
-  _id: '62852225b57ca6d7412909e3',
+  _id: mongoose.Types.ObjectId('62852225b57ca6d7412909e3'),
   taskName: 'Use of CSS in HTML',
   taskDescription: 'Use CSS to stylish the page',
   status: 'Finished',
