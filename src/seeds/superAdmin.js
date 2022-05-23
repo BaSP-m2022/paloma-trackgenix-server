34e883376db5fea5300780c20b8685173018ef04
Carla Daiana Vigil <carvigil21@gmail.com>
2022-05-23 10:00:04 -0300

    TG-46: added seeds and app.js file

---
+++ src/seeds/superAdmin.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('628517e345db451bcf6cdd08'),
+  name: 'juan',
+  lastName: 'miller',
+  email: 'juanmiller@gmail.com',
+  password: 'qw123123',
+},
+{
+  _id: mongoose.Types.ObjectId('628262eb0736fab270eba556'),
+  name: 'Cristina',
+  lastName: 'Lopez',
+  email: 'clopez@gmail.com',
+  password: 'asd123123',
+}];
