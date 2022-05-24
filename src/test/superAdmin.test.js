dd370ca0000acc0b55abd10f9091cb5c243e33f5
Carla Daiana Vigil <carvigil21@gmail.com>
2022-05-24 11:30:48 -0300

    TG-39: Added test for PUT and DELETE

--- src/test/superAdmin.test.js
+++ src/test/superAdmin.test.js
@@ -98,3 +98,92 @@ describe('GET By ID /superadmins', () => {
     expect(response.body.error).toBeTruthy();
   });
 });
+
+describe('PUT /superadmins', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).put(`/superadmins/${id}`).send({
+      name: 'Juan',
+      lastName: 'Miller',
+      email: 'juanmiller@gmail.com',
+      password: 'asd2022',
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).put(`/superadmins/${id}`).send({
+      name: 'Juan',
+      lastName: 'Miller',
+    });
+    expect(response.body.error).toBeFalsy();
+  });
+
+  test('Response should return a 404 status', async () => {
+    const response = await request(app).put('/superadmins/62825151c2380439abdfa145').send({
+      name: 'Juan',
+      lastName: 'Miller',
+      email: 'juanmiller@gmail.com',
+      password: 'asd2022',
+    });
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return message: \'The super admin has not been found\'', async () => {
+    const response = await request(app).put('/superadmins/62825151c2380439abdfa145').send({
+      name: 'Juan',
+      lastName: 'Miller',
+      email: 'juanmiller@gmail.com',
+      password: 'asd2022',
+    });
+    expect(response.body.message).toEqual('The super admin has not been found');
+  });
+
+  test('Response should return a 404 status - Missing ID', async () => {
+    const response = await request(app).put('/superadmins').send({
+      name: 'Juan',
+      lastName: 'Miller',
+      email: 'juanmiller@gmail.com',
+      password: 'asd2022',
+    });
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('DELETE /superadmins', () => {
+  test('Response should return a 200 status, error = false and message \'Deleted!\'', async () => {
+    const response = await request(app).delete(`/superadmins/${id}`).send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.msg).toEqual('Deleted!');
+  });
+
+  test('Response should return message: \'The super admin has not been found\'', async () => {
+    const response = await request(app).delete('/superadmins/628262eb0736fab270eba555').send();
+    expect(response.body.msg).toBe('The super admin has not been found');
+  });
+
+  test('Response should return an error', async () => {
+    const response = await request(app).delete('/superadmins/628262eb0736fab270eba555').send();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('Response should return status 404', async () => {
+    const response = await request(app).delete('/superadmins/').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return status 500', async () => {
+    const response = await request(app).delete('/superadmins/55').send();
+    expect(response.status).toBe(500);
+  });
+
+  test('Response should return an error', async () => {
+    const response = await request(app).delete('/superadmins/55').send();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('Response should return message: \'an error has ocurred\'', async () => {
+    const response = await request(app).delete('/superadmins/55').send();
+    expect(response.body.msg).toBe('an error has ocurred');
+  });
+});
