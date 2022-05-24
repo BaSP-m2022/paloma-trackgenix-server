0163343319b100d57b980c874d27684b5c887095
Hernan Cazeneuve <hernancazeneuve@gmail.com>
2022-05-24 18:40:38 -0300

    TG-41: Finished get test and getById, started put test

--- src/test/projects.test.js
+++ src/test/projects.test.js
@@ -18,8 +18,54 @@ describe('GET /Projects', () => {
     expect(response.error).toBe(false);
   });
 
-  test('Response should return false error status', async () => {
+  test('Response should have at least 1 object', async () => {
     const response = await request(app).get('/projects').send();
-    expect(response.error).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  test('The role of the employee should be QA ', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.body[0].employee[0].role).toBe('QA');
+  });
+
+  test('It should not get status 500', async () => {
+    const response = await request(app).get('/projects').send();
+    expect(response.status).not.toBe(500);
+  });
+});
+
+describe('GET /ProjectsById', () => {
+  test('Response should return a 404 status', async () => {
+    const response = await request(app).get('/projects/628bc4a3a60db4d71c872788').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).get('/projects/628bc4a3a60db4d71c872757').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return a 500 status', async () => {
+    const response = await request(app).get('/projects/628bc4a3a6').send();
+    expect(response.status).toBe(500);
+  });
+});
+
+describe('put /Projects', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/projects/628bc4a3a60db4d71c872757').send({
+      projectName: 'Project 2345',
+      totalHours: '48',
+      projectDescription: 'TEST Description 2',
+      startDate: '2022-03-12T03:00:00.000+00:00',
+      finishDate: '2022-05-01T03:00:00.000+00:00',
+      state: 'finished',
+      employee: [{
+        role: 'QA',
+        rate: '27',
+        employeeId: '628a6a4d9397655c03855a13',
+      }],
+    });
+    expect(response.status).toBe(200);
   });
 });
